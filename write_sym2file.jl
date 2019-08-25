e9b82d101de1138f7f8d0421f9f1555d3a9eba03
Vamsi <guruvamsi.policharla@gmail.com>
2019-08-26 00:58:25 +0530

    Changing approach for time ordering and removing magnus expansion approach files

--- write_sym2file.jl
+++
@@ -1,9 +0,0 @@
-open("A2.txt", "w") do f
-    for i in 1:20
-       for j in 1:20
-           write(f, string(A2_proj[i,j]))
-           write(f, " ")
-           end
-       write(f, ";\n")
-       end
-    end
