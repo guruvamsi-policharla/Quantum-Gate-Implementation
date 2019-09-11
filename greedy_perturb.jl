593291227bd045a4c7beeadc9f95f55706812d62
Vamsi <guruvamsi.policharla@gmail.com>
2019-09-11 19:23:58 +0530

    Fixed Parallelisation. Again. :)

--- greedy_perturb.jl
+++ greedy_perturb.jl
@@ -3,6 +3,7 @@ addprocs(4)
 println(nprocs())
 @everywhere using LinearAlgebra, SpecialFunctions, StatsBase, JLD2, Dates, SharedArrays, Statistics
 
+#@load "/home/vamsi/Github/Quantum-Gate-Implementation-master/data/fidelity_ts2019-09-11T22:35:54.6060.14_80_1500.jld2"
 @everywhere include("hamiltonian.jl")
 @everywhere include("EvalUT.jl")
 @everywhere include("DE.jl")
@@ -30,11 +31,28 @@ println(nprocs())
 ]
 @everywhere global const S = 0.0
 
-#=
-Fixing the units as follows
-ϵ is in Ghz
-T is in ns
-Hence, do not need to change h if we ignore the 10^9 factor in both time and frequency
-=#
 
-@time DE_iter()
+fidelity_arr = SharedArray{Float64,1}(DE_population)
+U = rand(DE_population,20,20)# + im*rand(DE_population,20,20)
+
+@sync @distributed for i in 1:DE_population
+    fidelity_arr[i] = fidelity(U[i,:,:],Utarget)
+    if i == DE_population
+        display(U[i,1:8,1:8])
+        println("fidelity",fidelity(U[i,:,:],Utarget))
+        display(Utarget)
+    end
+end
+
+fidelity_arr_temp = zeros(DE_population)
+for i in 1:DE_population
+    fidelity_arr_temp[i] = fidelity(U[i,:,:],Utarget)
+    if i == DE_population
+        display(U[i,1:8,1:8])
+        println("fidelity",fidelity(U[i,:,:],Utarget))
+        display(Utarget)
+    end
+end
+
+#println(fidelity_arr-fidelity_arr_temp)
+rmprocs(workers())
