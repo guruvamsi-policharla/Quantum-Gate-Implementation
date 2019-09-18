16a7e4ac9d5c34037ebb8c5df77c3e1136730d6d
Vamsi <guruvamsi.policharla@gmail.com>
2019-09-18 15:10:01 +0530

    Implemented phase compensation

--- main.jl
+++ main.jl
@@ -1,23 +1,23 @@
 using Distributed
 addprocs(4)
 println(nprocs())
-@everywhere using LinearAlgebra, SpecialFunctions, StatsBase, JLD2, Dates, SharedArrays, Statistics
+@everywhere using LinearAlgebra, SpecialFunctions, StatsBase, JLD2, Dates, SharedArrays, Statistics, Optim
 
 @everywhere include("hamiltonian.jl")
 @everywhere include("EvalUT.jl")
 @everywhere include("DE.jl")
 
-@everywhere global const N = 27
+@everywhere global const N = 30
 @everywhere global const knobs = 3
 @everywhere global const μl = 0.1
 @everywhere global const μu = 0.1
 @everywhere global const κ1 = 0.1
 @everywhere global const κ2 = 0.9
-@everywhere global const run_time = 27 #ns
+@everywhere global const run_time = 30 #ns
 @everywhere global const O3 = projector_gen()
 @everywhere global const comp_dim = 20
-@everywhere global const DE_population = 80
-@everywhere global const generations = 1000
+@everywhere global const DE_population = 10
+@everywhere global const generations = 5
 @everywhere global const Utarget = [
 1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
 0.0  1.0  0.0  0.0  0.0  0.0  0.0  0.0;
@@ -25,10 +25,10 @@ println(nprocs())
 0.0  0.0  0.0  1.0  0.0  0.0  0.0  0.0;
 0.0  0.0  0.0  0.0  1.0  0.0  0.0  0.0;
 0.0  0.0  0.0  0.0  0.0  1.0  0.0  0.0;
-0.0  0.0  0.0  0.0  0.0  0.0  0.0  1.0;
 0.0  0.0  0.0  0.0  0.0  0.0  1.0  0.0;
+0.0  0.0  0.0  0.0  0.0  0.0  0.0  -1.0;
 ]
-@everywhere global const S = 0.0
+@everywhere global const S = 0.14
 
 #=
 Fixing the units as follows
